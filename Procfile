f87edb095928d9faeb07f4de50b33182d4e7c0d6
Viktor M <vityok@gmail.com>
2018-11-02 13:11:33 +0200

    Prepared for Heroku push

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn test_django.wsgi --log-file -
--- emp_db/settings.py
+++ emp_db/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employees',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
