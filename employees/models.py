347c7aa62dcff1f2cbbc520e50338297e9f29ecd
Viktor M <vityok@gmail.com>
2018-11-02 13:34:48 +0200

    Adding model and admin

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    emp_number = models.IntegerField(blank=True, null=True)
+    first_name = models.CharField(max_length=250)
+    second_name = models.CharField(max_length=250)
+    dept_name = models.CharField(max_length=250)
+    project_name = models.CharField(max_length=250)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'Employee {self.first_name} {self.second_name} ({self.dept_name} of {self.project_name})'
+
