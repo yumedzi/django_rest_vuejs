65e3ea5cb012a1b7f49a2f8b1ebdbd32100436b8
Viktor M <vityok@gmail.com>
2018-11-05 16:14:27 +0200

    Adding VueJS app to get and post employees

--- emp_db/urls.py
+++ emp_db/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 from .router import router
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
